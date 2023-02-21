a76a3d9028d17ddf7d7847f4459522e51e85e466
shaokun <dan@movemnt.dev>
2023-02-21 22:13:45 +0800

    add aptos-vm script

--- aptos-vm/src/main.rs
+++ aptos-vm/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use aptos_vm::{AptosVM};
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).await?;
+    println!("The bytes: {}", buffer);
+    let count = AptosVM::get_num_proof_reading_threads();
+    println!("pool count: {}", count);
+    Ok(())
 }
